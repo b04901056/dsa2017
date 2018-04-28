70769c26f7ffd19e1e4ed0af32c83940e53dcff3
kevin <b04901056@ntu.edu.tw>
2018-04-28 16:19:00 +0800

    furfur

--- dlcv/hw3/hw3.py
+++ dlcv/hw3/hw3.py
@@ -16,62 +16,61 @@ from keras.callbacks import Callback
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 train_path = 'train'
 validation_path = 'validation'
-
+ 
 train_input = []
 val_input = []
 
 train_sat = []
 val_sat = []
 
+
 def rgb2label(filepath):
     file_list = [file for file in os.listdir(filepath) if file.endswith('.png')]
     file_list.sort()
     n_masks = len(file_list)
-    masks = np.empty((n_masks, 512, 512, 7),dtype='uint8')
-    for i, file in enumerate(file_list):    
-        if file[5]=='s': continue   
-        read_path = os.path.join(filepath, file)         
-        print(i)                                                                              
+    masks = np.empty((n_masks, 512, 512 ),dtype='uint8')
+    for i, file in enumerate(file_list): 
+        print(i)
+        read_path = os.path.join(filepath, file)                                                                        
         mask = scipy.misc.imread(read_path)                                                                        
         mask = (mask >= 128).astype(int)                                                                                              
         mask = 4 * mask[:, :, 0] + 2 * mask[:, :, 1] + mask[:, :, 2]                                                                  
-        masks[i, mask == 3] = [1,0,0,0,0,0,0]  # (Cyan: 011) Urban land                                                            
-        masks[i, mask == 6] = [0,1,0,0,0,0,0]  # (Yellow: 110) Agriculture land                                                          
-        masks[i, mask == 5] = [0,0,1,0,0,0,0]  # (Purple: 101) Rangeland                                                                  
-        masks[i, mask == 2] = [0,0,0,1,0,0,0]  # (Green: 010) Forest land                                                                
-        masks[i, mask == 1] = [0,0,0,0,1,0,0]  # (Blue: 001) Water                                                                        
-        masks[i, mask == 7] = [0,0,0,0,0,1,0]  # (White: 111) Barren land                                                                 
-        masks[i, mask == 0] = [0,0,0,0,0,0,1]  # (Black: 000) Unknown  
-        masks[i, mask == 4] = [0,0,0,0,0,0,1]
-    return masks.reshape(n_masks,512,512,7)
-'''
+        masks[i, mask == 3] = 0  # (Cyan: 011) Urban land                                                            
+        masks[i, mask == 6] = 1  # (Yellow: 110) Agriculture land                                                          
+        masks[i, mask == 5] = 2  # (Purple: 101) Rangeland                                                                  
+        masks[i, mask == 2] = 3  # (Green: 010) Forest land                                                                
+        masks[i, mask == 1] = 4  # (Blue: 001) Water                                                                        
+        masks[i, mask == 7] = 5  # (White: 111) Barren land                                                                 
+        masks[i, mask == 0] = 6  # (Black: 000) Unknown  
+        masks[i, mask == 4] = 6
+    return masks
+
 train_masks = rgb2label(train_path)
-print('train_masks size = ',train_masks.shape)
+#print('train_masks size = ',train_masks.shape)
 np.save('train_label.npy',train_masks)
+
 val_masks = rgb2label(validation_path)
-print('val_masks size = ',val_masks.shape)
+#print('val_masks size = ',val_masks.shape)
 np.save('val_label.npy',val_masks)
-train = os.listdir(train_path)
-for x in train:
-    if(x[5]=='m'): continue 
-    print(x) 
+
+train = [file for file in os.listdir(train_path) if file.endswith('.jpg')]
+for x in train:  
     read_path = os.path.join(train_path,x)
+    print(read_path)
     tmp = io.imread(read_path) 
     train_sat.append(tmp)
-train_input = np.array(train_sat,dtype='uint8')
-print('train_input size = ',train_input.shape)
+train_input = np.array(train_sat)
 np.save('train_input.npy',train_input)
-val = os.listdir(validation_path)
+
+val = [file for file in os.listdir(validation_path) if file.endswith('.jpg')]
 for x in val:
-    if(x[5]=='m'): continue
-    print(x)
     read_path = os.path.join(validation_path,x)
+    print(read_path)
     tmp = io.imread(read_path)
     val_sat.append(tmp)
-val_input = np.array(val_sat,dtype='uint8')
-print('val_input size = ',val_input.shape)
+val_input = np.array(val_sat)
 np.save('val_input',val_input)
-'''
+
 train_input = np.load('train_input.npy')
 print('train_input shape = ',train_input.shape)
 train_label = np.load('train_label.npy')
@@ -88,7 +87,7 @@ class EpochSaver(Callback):
         self.model = model 
 
     def on_epoch_end(self, epoch, logs={}):
-        if epoch==1 or epoch==10 or epoch==20 or epoch==40 or epoch =60\
+        if epoch==1 or epoch==10 or epoch==20 or epoch==40 or epoch ==60\
                     or epoch==80 or epoch==100:
             name = 'model_epoch_' + str(epoch) + '.h5'
             self.model.save(name) 
@@ -105,6 +104,8 @@ x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool' )(x)
 x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1' )(x)
 x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2' )(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool' )(x)
+x = BatchNormalization()(x)
+
 #f2 = x
 
 
@@ -127,6 +128,7 @@ x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1' )
 x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2' )(x)
 x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3' )(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool' )(x)
+x = BatchNormalization()(x)
 #f5 =
 
 o = x
@@ -135,8 +137,9 @@ o = ( Conv2D( 4096 , ( 3 , 3 ) , activation='relu' , padding='same'))(o)
 o = Dropout(0.5)(o)
 o = ( Conv2D( 4096 , ( 1 , 1 ) , activation='relu' , padding='same'))(o)
 o = Dropout(0.5)(o)
+o = BatchNormalization()(o)
 
-o = ( Conv2D( 7 ,  ( 1 , 1 ) , kernel_initializer='he_normal' ))(o)
+o = ( Conv2D( 7 ,  ( 1 , 1 ) , padding = 'valid' , kernel_initializer='he_normal' ))(o)
 o = Conv2DTranspose( 7,  
                      kernel_size=(64,64) ,  
 										 strides=(32,32) ,  
@@ -162,10 +165,10 @@ model.compile(loss=loss_fn,
                   metrics=['accuracy'])
 model.fit(train_input,
           train_label, 
-          batch_size = 10, 
-          epochs= 40, 
+          batch_size = 6, 
+          epochs= 100, 
           verbose= 1,
           validation_data = (val_input,val_label), 
           callbacks=[EpochSaver(model)])
-
+ 
 model.save('model_epoch_100.h5')
