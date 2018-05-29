7f4eb0ab598fde3a9ec6d09ed71a14bfca0bbfd9
kevin <b04901056@ntu.edu.tw>
2018-05-29 22:30:14 +0800

    furfur

---
+++ dlcv/hw5/5_1_model.py
@@ -0,0 +1,365 @@
+import scipy.misc
+import numpy as np
+import torch
+from PIL import Image
+from torch.utils.data import Dataset,DataLoader
+import os
+from torch.autograd import Variable
+import torch.nn as nn
+import argparse 
+import torch.optim as optim
+import torchvision
+import random , time , math
+from tensorboardX import SummaryWriter 
+
+parser =  argparse.ArgumentParser(description='5_1 model')
+parser.add_argument('-b',type=int,dest='batch_size',required=True)
+parser.add_argument('-e',type=int,dest='epoch',required=True)
+parser.add_argument('-trn',type=int,dest='train_num',required=True)
+parser.add_argument('-exp',type=str,dest='exp',required=True)
+args = parser.parse_args()
+writer = SummaryWriter('runs/exp_'+args.exp)
+
+class imagedataset(Dataset):
+    def __init__(self,img,label):
+        self.img = img
+        self.label = label 
+    def __getitem__(self,i):
+        x = self.img[i]
+        y = self.label[i]
+        return x,y 
+    def __len__(self):
+        return self.img.shape[0]
+
+def get_data(img,label,batch_size,shuffle=False):
+    img = np.load(img)
+    label = np.load(label)
+    data = []
+    for x in img:
+        half = int(x.shape[0]/2)
+        last = x.shape[0]-1
+        a = x[0].reshape(1,-1)
+        b = x[half].reshape(1,-1)
+        c = x[last].reshape(1,-1)
+        data.append(np.concatenate((a,b,c),axis=1))
+    data = np.array(data)
+    print('data.shape:',data.shape)
+    #input()
+    data = torch.FloatTensor(data)
+    label = torch.FloatTensor(label)
+        
+    dataset = imagedataset(data,label)
+    return DataLoader(dataset,batch_size=batch_size,shuffle=shuffle)
+
+class fcn(nn.Module):
+    def __init__(self):
+        super(fcn,self).__init__()
+        self.dnn1  = nn.Linear(3000,1024)
+        self.dnn2  = nn.Linear(1024,256)
+        self.dnn3  = nn.Linear(256,128)
+        self.dnn4  = nn.Linear(128,64)
+        self.dnn5  = nn.Linear(64,11)
+        self.softmax = nn.Softmax(dim=1)
+        self.dropout = nn.Dropout(p=0.25)
+
+    def forward(self,x):
+        h1 = self.dropout(self.dnn1(x))
+        h2 = self.dnn2(h1)
+        h3 = self.dropout(self.dnn3(h2))
+        h4 = self.dnn4(h3) 
+        h5 = self.dropout(self.dnn5(h4))
+        h6 = self.softmax(h5)
+        return h6
+
+model = fcn().cuda()
+print(model)
+print('-'*50) 
+optimizer = optim.Adam(model.parameters(), lr=0.0001, betas=(0.5, 0.999))
+criterion = torch.nn.CrossEntropyLoss()
+
+training_set = get_data(img='video_data_train.npy',label='video_label_train.npy'\
+                        ,batch_size=args.batch_size,shuffle=True)
+validation_set = get_data(img='video_data_val.npy',label='video_label_val.npy'\
+                        ,batch_size=args.batch_size,shuffle=False)
+print('saved data ...')
+print('start training ...')
+#input()
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def train_iter(epoch,model,iteration): 
+    model.train()
+    start = time.time()
+    loss = 0
+    accuracy = 0
+    for step , (batch_x,batch_y) in enumerate(training_set):
+        batch_idx = step + 1 
+        batch_x = Variable(batch_x.squeeze(1)).cuda() 
+        batch_size = len(batch_x) 
+
+        label = Variable(batch_y.long(),volatile=True).cuda()  
+        optimizer.zero_grad()  
+        output = model(batch_x) 
+        train_loss = criterion(output,label)
+        train_loss.backward()
+        optimizer.step()
+        output = output.data.cpu().numpy()
+        answer = np.argmax(output,axis=1) 
+
+        count = 0
+        for i in range(batch_size):
+            if answer[i] == batch_y[i]: count += 1
+        accu = count/batch_size 
+
+        loss += train_loss.data[0]
+        accuracy += accu
+        writer.add_scalar('Training loss', train_loss.data[0] , iteration)
+        writer.add_scalar('Training accuracy', accu , iteration)
+        '''
+        print('\rTrain Epoch: {} [{}/{} ({:.0f}%)] | Accu: {:.6f} | | Loss: {:.6f} | step: {} | Time: {} '.format(
+                   epoch 
+                   , batch_idx * batch_size 
+                   , len(training_set.dataset) 
+                   , 100. * batch_idx * batch_size / len(training_set.dataset)
+                   , accu
+                   , train_loss.data[0]
+                   , iteration
+                   , timeSince(start, batch_idx* batch_size / len(training_set.dataset)))
+                   , end='')
+        '''
+        iteration += 1 
+     
+    print('\n====> Training Epoch : {} | Time: {} | loss: {:.4f} | Accu: {:.4f} '.format(
+                epoch 
+                , timeSince(start,1)
+                , loss / len(training_set)
+                , accuracy / len(training_set) ))
+    return iteration
+
+def val(epoch,model,iteration): 
+    model.eval()
+    start = time.time()
+    loss = 0
+    accuracy = 0
+    for step , (batch_x,batch_y) in enumerate(validation_set):
+        batch_idx = step + 1 
+        batch_x = Variable(batch_x.squeeze(1)).cuda() 
+        batch_size = len(batch_x) 
+        
+        label = Variable(batch_y.long(),volatile=True).cuda()  
+        #optimizer.zero_grad()  
+        output = model(batch_x) 
+        val_loss = criterion(output,label)
+        #train_loss.backward()
+        #optimizer.step()
+        output = output.data.cpu().numpy()
+        answer = np.argmax(output,axis=1) 
+
+        count = 0
+        for i in range(batch_size):
+            if answer[i] == batch_y[i]: count += 1
+        accu = count/batch_size 
+
+        loss += val_loss.data[0]
+        accuracy += accu
+        writer.add_scalar('Validation loss', val_loss.data[0] , iteration)
+        writer.add_scalar('Validation accuracy', accu , iteration)
+        '''
+        print('\rValidation Epoch: {} [{}/{} ({:.0f}%)] | Accu: {:.6f} | | Loss: {:.6f} | step: {} | Time: {} '.format(
+                   epoch 
+                   , batch_idx * batch_size 
+                   , len(validation_set.dataset) 
+                   , 100. * batch_idx * batch_size / len(validation_set.dataset)
+                   , accu
+                   , val_loss.data[0]
+                   , iteration
+                   , timeSince(start, batch_idx* batch_size / len(validation_set.dataset)))
+                   , end='')
+        '''
+        iteration += 1
+        
+    print('\n====> Validation Epoch : {} | Time: {} | loss: {:.4f} | Accu: {:.4f} \n'.format(
+                epoch 
+                , timeSince(start,1)
+                , loss / len(validation_set)
+                , accuracy / len(validation_set) ))
+    return iteration
+
+for i in range(args.epoch):
+    epoch = i+1
+    train_iter(epoch,model,(epoch-1)*len(training_set))
+    val(epoch,model,(epoch-1)*len(validation_set))
+    if epoch % 30 == 0:
+        torch.save(model,'model/5-1/model_'+str(epoch)+'.pt')
+
+# test validation set performance
+
+#model = torch.load('model/5-1/model_990.pt')
+#val(10,model,(10-1)*len(validation_set))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dlcv/hw5/5_1_preprocess.py
@@ -0,0 +1,87 @@
+import numpy as np 
+import reader
+from torchvision import models
+import torchvision
+from torch.autograd import Variable
+from PIL import Image
+import sys
+import os
+import torch
+
+video_path_train = 'data/TrimmedVideos/video/train'
+video_path_val = 'data/TrimmedVideos/video/valid'
+
+data = reader.getVideoList('data/TrimmedVideos/label/gt_train.csv')
+dic = {}
+name = data['Video_name']
+label = data['Action_labels']
+#print('label:',label)
+#input()
+for i in range(len(label)): 
+    dic[name[i]] = label[i] 
+
+model = models.vgg16(pretrained=True)
+model.eval()
+model.cuda()
+
+video_data = []
+video_label = []
+ 
+for cat in os.listdir(video_path_train):
+    path = os.path.join(video_path_train,cat)
+    for x in os.listdir(path):
+        video_label.append(dic[x[:-20]])
+        print(x[:-20])
+        print(dic[x[:-20]])
+        #input()
+        a = reader.readShortVideo(video_path_train,cat,x)
+        #print(a.shape)
+        #input()
+        #print(model)
+        #input() 
+        img = []
+        for i in range(a.shape[0]):
+            b = a[i]
+            b = Image.fromarray(b)
+            #print(b)
+            #input()
+            new_im = b.resize((224, 224), Image.BICUBIC)
+            #print(new_im)
+            #input()
+            new_im = torchvision.transforms.ToTensor()(new_im)
+            new_im = torch.unsqueeze(new_im,0)
+            #img.append(torch.unsqueeze(new_im,0))
+            #img = torch.cat(img,0)
+            new_im = Variable(new_im,volatile=True).cuda()
+            #print(img)
+            #input()
+            c = model(new_im)
+            #print(c)
+            #input()
+            c = c.data.cpu().numpy()
+            torch.cuda.empty_cache()
+            img.append(c)
+        img = np.concatenate(img,axis=0)
+        #print(img)
+        #print(img.shape)
+        #input()
+        video_data.append(img)
+			
+video_data = np.array(video_data)
+video_label = np.array(video_label).astype(int)
+print('video_label:',video_label)
+input()
+np.save('video_data_train.npy',video_data)
+np.save('video_label_train.npy',video_label)
+print('data saved ...')
+'''
+video_data = np.load('video_data.npy')
+vidoe_label = np.load('vidoe_label.npy')
+video_number = np.load('video_number.npy')
+for i in range(len(video_data)):
+	print(video_data[i].shape)
+input()
+print(video_data.shape)
+print(vidoe_label.shape)
+print(video_number.shape)
+'''
